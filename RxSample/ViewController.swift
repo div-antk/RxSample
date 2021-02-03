399d2ced04c3e88333765bffcceaf84e73a23381
div-antk <51283468+owaii@users.noreply.github.com>
2021-02-03 22:25:35 +0900

    実装

--- RxSample/ViewController.swift
+++ RxSample/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // textFieldに入力するとLabelに反映される
+        textField.rx.text.orEmpty
+            .bind(to: label.rx.text)
+            .disposed(by: disposeBag)
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
