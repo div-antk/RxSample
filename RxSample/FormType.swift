52d8e1f9574b0bc5f4ee9b01dc216064d69dc98f
div-antk <51283468+owaii@users.noreply.github.com>
2021-02-05 22:20:56 +0900

    enumの実装、バリデーションメッセージなど

--- RxSample/FormType.swift
+++ RxSample/FormType.swift
@@ -22,4 +22,49 @@ enum FormType: Int, CaseIterable {
             return "password"
         }
     }
+    
+    // 出現するキーボードのタイプ
+    var keyboardType: UIKeyboardType {
+        switch self {
+        case .email:
+            return .emailAddress
+        case .userName, .password:
+            return .asciiCapable
+        }
+    }
+    
+    // 伏せ字にするかどうか
+    var isSecureTextEntry: Bool {
+        switch self {
+        case .userName, .email:
+            return false
+        case .password:
+            return true
+        }
+    }
+    
+    // 入力補完
+    var textContentType: UITextContentType {
+        switch self {
+        case .userName:
+            return .username
+        case .email:
+            return .emailAddress
+        case .password:
+            return .newPassword
+            
+        }
+    }
+    
+    var caution: String {
+        switch self {
+        case .userName:
+            return "半角英数字6文字以上にしてください"
+        case .email:
+            return "メールアドレスが不正です"
+        case .password:
+            return "半角数字、半角英大文字、半角英小文字を含んでください"
+        }
+    }
+        
 }
